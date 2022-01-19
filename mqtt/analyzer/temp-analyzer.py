83b16c400560052da50c1df3e79f5ffbb35883dc
smartx-usman <usman@smartx.kr>
2022-01-19 11:55:57 +0100

    Update default values. Add functionality for float number values generation. Check if data file already exists.

--- mqtt/analyzer/temp-analyzer.py
+++ mqtt/analyzer/temp-analyzer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import logging
 import os
+import sys
 import time
 
 import faust
@@ -15,11 +16,35 @@ mqtt_client_id = f'mqtt-faust-analyzer'
 kafka_broker = 'kafka://' + os.environ['KAFKA_BROKER']
 kafka_topic = os.environ['KAFKA_TOPIC']
 kafka_key = "server-room"
-temperature_file = open("/analyzer/temperature-data.csv", "a")
-invalid_data_file = open("/analyzer/temperature-invalid-data.csv", "a")
+value_type = os.environ['VALUE_TYPE']
+data_file_name = "/analyzer/temperature-data.csv"
 actuator_id = 'actuator-0'
 actuator_actions = ['power-on', 'pause', 'shutdown']
 
+
+# Cast values to correct type
+if value_type == 'integer':
+    min_threshold_value = int(os.environ['MIN_THRESHOLD_VALUE'])
+    max_threshold_value = int(os.environ['MAX_THRESHOLD_VALUE'])
+    invalid_value = int(os.environ['INVALID_VALUE'])
+elif value_type == 'float':
+    min_threshold_value = float(os.environ['MIN_THRESHOLD_VALUE'])
+    max_threshold_value = float(os.environ['MAX_THRESHOLD_VALUE'])
+    invalid_value = float(os.environ['INVALID_VALUE'])
+
+
+# Remove old data file from persistent volume
+if os.path.exists(data_file_name):
+    os.remove(data_file_name)
+    logging.info('Removed old file from the PV.')
+else:
+    logging.info('The file does not exist.')
+
+# Open data file for writing
+try:
+    temperature_file = open(data_file_name, "a")
+except Exception as ex:
+    logging.error(f'Exception while opening file {temperature_file}.', exc_info=True)
 
 # Connect to MQTT broker
 def connect_to_mqtt():
@@ -46,7 +71,7 @@ def mqtt_publish_message(mqtt_publisher, message):
     status = result[0]
 
     if status == 0:
-        logging.info(f"Send `{message}` to topic `{mqtt_topic}`")
+        logging.info(f"Send {message} to topic `{mqtt_topic}`")
     else:
         logging.error(f"Failed to send message to topic {mqtt_topic}")
 
@@ -56,17 +81,25 @@ client = connect_to_mqtt()
 
 # Parse message for MQTT
 def parse_message_for_actuator(reading_ts, actuator, action):
-    logging.info(f'{action} heating system.')
+    logging.info(f'{action} heating system action is generated.')
     message = f"reading_ts:{reading_ts} actuator_id:{actuator} action:{action}"
     mqtt_publish_message(client, message)
 
 
 # Create a class to parse message from Kafka
-class Temperature(faust.Record, serializer='json'):
-    reading_ts: int
-    sensor: str
-    value: int
-
+if value_type == 'integer':
+    class Temperature(faust.Record, serializer='json'):
+        reading_ts: int
+        sensor: str
+        value: int
+elif value_type == 'float':
+    class Temperature(faust.Record, serializer='json'):
+        reading_ts: int
+        sensor: str
+        value: float
+else:
+    logging.critical(f'Invalid value type {value_type} is provided. Exiting.')
+    sys.exit()
 
 app = faust.App('temp-analyzer', broker=kafka_broker, )
 topic = app.topic(kafka_topic, value_type=Temperature)
@@ -77,23 +110,36 @@ topic = app.topic(kafka_topic, value_type=Temperature)
 async def check(temperatures):
     async for temperature in temperatures:
         start_time = time.perf_counter()
-        logging.info(f'Reading {temperature.value} at ts {temperature.reading_ts} from {temperature.sensor}')
+        logging.info(f'Reading: {temperature.value} Timestamp: {temperature.reading_ts} Sensor: {temperature.sensor}')
 
-        if int(temperature.value) == 99:
-            logging.info('Anomalous value found. Discarded and recorded in file.')
-            invalid_data_file.write(temperature.reading_ts + "," + temperature.sensor + "," + temperature.value + "\n")
-        else:
-            temperature_file.write(temperature.reading_ts + "," + temperature.sensor + "," + temperature.value + "\n")
-            if int(temperature.value) < 3:
-                parse_message_for_actuator(temperature.reading_ts, actuator_id, actuator_actions[0])
-            elif int(temperature.value) > 8:
-                parse_message_for_actuator(temperature.reading_ts, actuator_id, actuator_actions[2])
+        # Write data to a file
+        temperature_file.write(temperature.reading_ts + "," + temperature.sensor + "," + temperature.value + "\n")
+
+        # Create some checks on incoming data to create actuator actions
+        if value_type == 'integer':
+            if int(temperature.value) == invalid_value:
+                logging.warning('Anomalous value found. Value is discarded.')
             else:
-                logging.info('No action required.')
+                if int(temperature.value) < min_threshold_value:
+                    parse_message_for_actuator(temperature.reading_ts, actuator_id, actuator_actions[0])
+                elif int(temperature.value) > max_threshold_value:
+                    parse_message_for_actuator(temperature.reading_ts, actuator_id, actuator_actions[2])
+                else:
+                    logging.info('No action required.')
+        elif value_type == 'float':
+            if float(temperature.value) == invalid_value:
+                logging.warning('Anomalous value found. Value is discarded.')
+            else:
+                if float(temperature.value) < min_threshold_value:
+                    parse_message_for_actuator(temperature.reading_ts, actuator_id, actuator_actions[0])
+                elif float(temperature.value) > max_threshold_value:
+                    parse_message_for_actuator(temperature.reading_ts, actuator_id, actuator_actions[2])
+                else:
+                    logging.info('No action required.')
 
         end_time = time.perf_counter()
         time_ms = (end_time - start_time) * 1000
-        logging.info(f'It took {time_ms} ms to process the message.')
+        logging.info(f'Message processing took {time_ms} ms.')
 
 
 if __name__ == '__main__':
