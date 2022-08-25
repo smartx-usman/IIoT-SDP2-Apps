3c6b3d08dbbbb5105e02a5ed67c5db9c6afd67ab
smartx-usman <usman@smartx.kr>
2022-08-25 16:12:50 +0200

    Add interface and classes to simplify storage functionality

---
+++ mqtt/analyzer/file_store.py
@@ -0,0 +1,40 @@
+import logging
+import os
+
+from data_store import DataStore
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.WARN)
+
+
+class FileStore(DataStore):
+    def connect_db(self):
+        pass
+
+    def open_file(self, data_file_normal, data_file_anomalous):
+        # Remove old data file from persistent volume
+        if os.path.exists(data_file_normal):
+            os.remove(data_file_normal)
+            logging.info('Removed old file from the PV.')
+        else:
+            logging.info('The file does not exist.')
+
+        if os.path.exists(data_file_anomalous):
+            os.remove(data_file_anomalous)
+            logging.info('Removed old file from the PV.')
+        else:
+            logging.info('The file does not exist.')
+
+        # Open data file for writing
+        try:
+            temperature_file_normal = open(data_file_normal, "a")
+        except Exception as ex:
+            logging.error(f'Exception while opening file {temperature_file_normal}.', exc_info=True)
+
+        try:
+            temperature_file_anomalous = open(data_file_anomalous, "a")
+        except Exception as ex:
+            logging.error(f'Exception while opening file {temperature_file_anomalous}.', exc_info=True)
+        return temperature_file_normal, temperature_file_anomalous
+
+    def store_data(self, session, file_handler, reading_ts, process_ts, sensor, value):
+        file_handler.write(str(reading_ts) + "," + str(process_ts) + "," + sensor + "," + value + "\n")
