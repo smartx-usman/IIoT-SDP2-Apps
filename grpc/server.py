79c8c95f5da13d4c4d298e360c50108cf7a4823e
smartx-usman <usman@smartx.kr>
2022-08-26 11:50:29 +0200

    Add functionality to read/write multiple messages to file.

--- grpc/server.py
+++ grpc/server.py
@@ -1,17 +1,17 @@
 from concurrent import futures
 
 import grpc
-import greeting_pb2
+import greeting_pb2 as pb
 import greeting_pb2_grpc
 
 
 class Greeter(greeting_pb2_grpc.GreeterServicer):
     def greet(self, request, context):
         print("Got request " + str(request))
-        return greeting_pb2.ServerOutput(message='{0} {1}!'.format(request.greeting, request.name))
+        return pb.ServerOutput(message='{0} {1}!'.format(request.greeting, request.name))
 
 
-def server():
+def start_server():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
     greeting_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
     server.add_insecure_port('[::]:5051')
@@ -20,4 +20,4 @@ def server():
     server.wait_for_termination()
 
 
-server()
+start_server()
