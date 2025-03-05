bcd33390a28539148b13fe504da15c6428ae98af
smartx-usman <usman@smartx.kr>
2025-03-05 15:16:14 +0100

    Add quotas when registering new users.

--- workload-manager/app/routes/auth_routes.py
+++ workload-manager/app/routes/auth_routes.py
@@ -1,9 +1,11 @@
 import logging
+import re
 
 from flask import Blueprint, jsonify, request, render_template, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user
 from app import db, login_manager
 from app.models import User
+from app.kubernetes_utils import ensure_user_namespace
 from werkzeug.security import check_password_hash, generate_password_hash
 
 auth_bp = Blueprint('auth', __name__)
@@ -12,7 +14,6 @@ auth_bp = Blueprint('auth', __name__)
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-    # return db.session.get(User, int(user_id))
 
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
@@ -49,11 +50,22 @@ def register():
             role = request.form.get('role')
             level = request.form.get('level')
             hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
-            new_user = User(username=username, password=hashed_password, role=role, user_level=level)
+            namespace = f"user-{re.sub(r'[^a-z0-9-]', '', username.lower())[:45]}"
+            quota_pods = request.form.get('quota_pods')
+            quota_cpu = request.form.get('quota_cpu')
+            quota_memory = request.form.get('quota_memory')
+
+            new_user = User(username=username, password=hashed_password, role=role, user_level=level,
+                            namespace=namespace, quota_pods=quota_pods, quota_cpu=quota_cpu, quota_memory=quota_memory)
+
             db.session.add(new_user)
             db.session.commit()
-            logging.info(
-                f'New user registered successfully. Username: {username}, Role: {role}, Expertise-level: {level}')
+
+            ensure_user_namespace(username=username, namespace=namespace, quota_pods=quota_pods, quota_cpu=quota_cpu,
+                                  quota_memory=quota_memory)
+
+            logging.info(f'User registered: Username: {username}, Role: {role}, Expertise-level: {level}')
             return jsonify({"status": "success", "message": "New user registered successfully."})
     except Exception as e:
+        db.session.rollback()
         return jsonify({"status": "error", "message": str(e)})
