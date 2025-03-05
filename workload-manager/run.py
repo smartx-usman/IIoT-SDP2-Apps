e602e8adbeefa99ad73b8fc101da529922825855
smartx-usman <usman@smartx.kr>
2025-03-05 15:21:34 +0100

    Set ssl context to use http.

--- workload-manager/run.py
+++ workload-manager/run.py
@@ -4,5 +4,4 @@ from config import Config
 app = create_app(Config)
 
 if __name__ == '__main__':
-    #  initialize_workload_types()
-    app.run(host='0.0.0.0', port=9000, debug=False)
+    app.run(host='0.0.0.0', port=9000, debug=False, ssl_context=None)
