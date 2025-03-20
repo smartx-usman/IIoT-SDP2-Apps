b8c902a4196c76ca16e35678b2bed314ce988060
smartx-usman <usman@smartx.kr>
2025-03-20 15:12:37 +0100

    Add back mistakenly deleted variable.

--- workload-manager/config.py
+++ workload-manager/config.py
@@ -5,6 +5,7 @@ class Config:
     SECRET_KEY = 'admin_secret_key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
     KUBECONFIG_PATH = '/home/.kube/config'
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', './uploads')
     GRAFANA_URL = os.environ.get('GRAFANA_URL')
     GRAFANA_ADMIN_USER = os.environ.get('GRAFANA_ADMIN_USER')
     GRAFANA_ADMIN_PASSWORD = os.environ.get('GRAFANA_ADMIN_PASSWORD')
