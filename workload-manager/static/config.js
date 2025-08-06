6d649a78d39d5fad68611ffcba55b60d6888b7a7
smartx-usman <usman@smartx.kr>
2025-08-06 13:11:23 +0200

    Correct kinds order for proper option display.

--- workload-manager/static/config.js
+++ workload-manager/static/config.js
@@ -1,13 +1,13 @@
 const FIELDS_CONFIG = {
     'Pod': {
         beginner: ['image', 'command', 'args', 'port'],
-        intermediate: ['image', 'command', 'args', 'port', 'env', 'imagePullPolicy', 'resources', 'volumes'],
-        advanced: ['image', 'command', 'args', 'port', 'env', 'imagePullPolicy', 'resources', 'volumes', 'restartPolicy', 'affinity', 'toleration', 'securityContext', 'probes']
+        intermediate: ['image', 'command', 'args', 'port', 'imagePullPolicy', 'env', 'resources', 'volumes'],
+        advanced: ['image', 'command', 'args', 'port', 'imagePullPolicy', 'env', 'resources', 'volumes', 'restartPolicy', 'affinity', 'toleration', 'securityContext', 'probes']
     },
     'Deployment': {
         beginner: ['image', 'command', 'args', 'port', 'replicas'],
-        intermediate: ['image', 'command', 'args', 'port', 'replicas', 'env', 'imagePullPolicy', 'resources', 'volumes'],
-        advanced: ['image', 'command', 'args', 'port', 'replicas', 'env', 'imagePullPolicy', 'resources', 'volumes', 'restartPolicy', 'affinity', 'toleration', 'securityContext', 'probes']
+        intermediate: ['image', 'command', 'args', 'port', 'replicas', 'imagePullPolicy', 'env', 'resources', 'volumes'],
+        advanced: ['image', 'command', 'args', 'port', 'replicas', 'imagePullPolicy', 'env', 'resources', 'volumes', 'restartPolicy', 'affinity', 'toleration', 'securityContext', 'probes']
     },
     'Job': {
         beginner: ['image', 'command'],
