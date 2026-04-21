bf07c8a1610158a2c0038cf98ad89f0cce9db77b
smartx-usman <usman@smartx.kr>
2026-04-21 10:59:21 +0200

    Remove unused files and code.

--- monitoring-anomly/file-app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-kubectl apply -f /home/aida/IIoT-SDP2-Apps/file-server/file-server.yaml
-sleep 10
-kubectl apply -f /home/aida/IIoT-SDP2-Apps/file-server/file-client.yaml
--- monitoring-anomly/web-app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-kubectl apply -n flask-server -f /home/aida/IIoT-SDP2-Apps/web-server/flask.yaml
-sleep 10
-kubectl apply -n flask-server -f /home/aida/IIoT-SDP2-Apps/web-server/client.yaml
